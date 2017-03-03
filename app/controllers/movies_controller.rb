6c432d2d748fa1dd58965e7fe2fe2231daa36f15
Jesse Pinkston <jpinkst3@uncc.edu>
2017-03-03 16:50:03 +0000

    Done with section 4.6 update

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,8 +3,19 @@ class MoviesController < ApplicationController
   def index
     @movies = Movie.all
   end
-end
   def new
     @movie = Movie.new
     # default: render 'new' template
   end
+  def create
+    #@movie = Movie.create!(params[:movie]) #old way
+    @movie = Movie.create!(movie_params)  # new way
+    flash[:notice] = "#{@movie.title} was successfully created."
+    redirect_to movies_path
+  end
+  
+  private
+    def movie_params
+      params.require(:movie).permit(:title, :rating, :description, :release_date)
+    end
+end
