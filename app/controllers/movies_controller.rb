cd21e61cb0762a2b1dae169f7ea711bd111583ea
Jesse Pinkston <jpinkst3@uncc.edu>
2017-03-03 17:49:28 +0000

    Done with section 4.8

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,10 @@
 # This file is app/controllers/movies_controller.rb
 class MoviesController < ApplicationController
+  def show
+    id = params[:id] # retrieve movie ID from URI route
+    @movie = Movie.find(id) # look up movie by unique ID
+    # will render app/views/movies/show.<extension> by default
+  end
   def index
     @movies = Movie.all
   end
@@ -13,12 +18,21 @@ class MoviesController < ApplicationController
     flash[:notice] = "#{@movie.title} was successfully created."
     redirect_to movies_path
   end
+  def edit
+  @movie = Movie.find params[:id]
+  end
   def update
     @movie = Movie.find params[:id]
     #@movie.update_attributes!(params[:movie])  # old way
     @movie.update_attributes!(movie_params)  # new way  
     flash[:notice] = "#{@movie.title} was successfully updated."
     redirect_to movie_path(@movie)
+  end
+  def destroy
+    @movie = Movie.find(params[:id])
+    @movie.destroy
+    flash[:notice] = "Movie '#{movie.title}' deleated."
+    redirect_to movies_path
   end
   
   private
